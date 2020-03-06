926ae78630f9a09a3f4117667ca0c3c483371479
48lika48 <48lika48@mail.ru>
2020-03-06 14:26:34 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function toReadable (number) {
-  
+    
+    let units = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let dozens = ["hundred", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+    let arr = String(number).split("");
+    
+    if (number < 20) {
+        return units[number];
+    }
+    else if (arr.length == 2 && arr[1] == 0) {
+        return dozens[arr[0]];
+    }
+    else if (arr.length == 2 && arr[1] !== 0) {
+        return dozens[arr[0]] + " " + units[arr[1]];
+    }
+    else if (arr.length == 3 && arr[1] == 0 && arr[2] == 0) {
+        return units[arr[0]] + " " + dozens[0];
+    }
+    else if (arr.length == 3 && arr[1] == 0) {
+        return units[arr[0]] + " " + dozens[0] + " " + units[arr[2]];
+    }
+    else if (arr.length == 3 && arr[1] == 1) {
+        return units[arr[0]] + " " + dozens[0] + " " + units[Number(arr[2])+10];
+    }
+    else if (arr.length == 3 && arr[2] == 0) {
+        return units[arr[0]] + " " + dozens[0] + " " + dozens[arr[1]];
+    }
+    else {
+        return units[arr[0]] + " " + dozens[0] + " " + dozens[arr[1]] + " " + units[arr[2]];
+    }
 }
